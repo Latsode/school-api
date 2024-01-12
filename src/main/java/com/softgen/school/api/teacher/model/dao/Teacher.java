af6e334b9d80824fe8ad8a3422a977267630d70e
Nikoloz <nikalatsabidze@gmail.com>
2024-01-12 19:58:26 +0400

    removes not needed annotations from lombok

--- src/main/java/com/softgen/school/api/teacher/model/dao/Teacher.java
+++ src/main/java/com/softgen/school/api/teacher/model/dao/Teacher.java
@@ -1,18 +1,17 @@
 package com.softgen.school.api.teacher.model.dao;
 
 import com.softgen.school.api.teacher.model.enums.TeacherStatus;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-@Data
+@Setter
+@Getter
 @Entity(name = "teacher")
 public class Teacher {
 
