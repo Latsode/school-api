b4b0cb1cc2dda83910a564328989525e6dffde39
Nikoloz <nikalatsabidze@gmail.com>
2024-01-12 19:48:32 +0400

    fixes lombok not needed annotations

--- src/main/java/com/softgen/school/api/student/model/dao/Student.java
+++ src/main/java/com/softgen/school/api/student/model/dao/Student.java
@@ -1,18 +1,17 @@
 package com.softgen.school.api.student.model.dao;
 
 import com.softgen.school.api.student.model.enums.StudentStatus;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import lombok.Data;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
-@Data
+@Getter
+@Setter
 @Entity(name = "student")
 public class Student {
 
