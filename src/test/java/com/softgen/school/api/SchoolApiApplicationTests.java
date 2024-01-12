4f289d4f3749073e4e5ee6fea6c767007818b691
Nikoloz <nikalatsabidze@gmail.com>
2024-01-12 16:45:57 +0400

    Initial

---
+++ src/test/java/com/softgen/school/api/SchoolApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softgen.school.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchoolApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
