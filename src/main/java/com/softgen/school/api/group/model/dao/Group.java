7bd04a0d697e420f2fd9f574be24acac02eb2915
Nikoloz <nikalatsabidze@gmail.com>
2024-01-12 21:34:41 +0400

    adds join table to handle student and teacher relationship to group

--- src/main/java/com/softgen/school/api/group/model/dao/Group.java
+++ src/main/java/com/softgen/school/api/group/model/dao/Group.java
@@ -1,7 +1,8 @@
 package com.softgen.school.api.group.model.dao;
 
 import com.softgen.school.api.group.model.enums.GroupStatus;
-import com.softgen.school.api.student.model.enums.StudentStatus;
+import com.softgen.school.api.student.model.dao.Student;
+import com.softgen.school.api.teacher.model.dao.Teacher;
 import jakarta.persistence.*;
 import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
@@ -9,6 +10,8 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 @Entity(name = "group_table")
@@ -31,6 +34,20 @@ public class Group {
     private LocalDateTime updatedAt;
 
     private GroupStatus status;
+
+    @ManyToMany
+    @JoinTable(
+            name = "group_student",
+            joinColumns = @JoinColumn(name = "group_id"),
+            inverseJoinColumns = @JoinColumn(name = "student_id"))
+    private Set<Student> students = new HashSet<>();
+
+    @ManyToMany
+    @JoinTable(
+            name = "group_teacher",
+            joinColumns = @JoinColumn(name = "group_id"),
+            inverseJoinColumns = @JoinColumn(name = "teacher_id"))
+    private Set<Teacher> teachers = new HashSet<>();
 
     @PrePersist
     public void prePersist() {
